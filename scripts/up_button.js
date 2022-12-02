355f66f51c3b7c884b9d2f368ba03d3b8d91099e
AAKVAS <aleksejplotniklv@gmail.com>
2022-12-02 10:44:42 +0300

    Сделал кнопку пролистывания вверх, немного зарефакторил

---
+++ scripts/up_button.js
@@ -0,0 +1,18 @@
+$(function () {
+    $("#up_button").load("../html_components/up_button.html");
+
+    $(window).scroll(function() {
+
+        if($(this).scrollTop() !== 0) {
+            $('#up_button').fadeIn();
+        } else {
+            $('#up_button').fadeOut();
+        }
+    });
+
+    $('#up_button').click(function() {
+
+        $('body,html').animate({scrollTop:0},100);
+
+    });
+});
